c244f06fac18d9f0d4666c7102390dc8b9e0a17f
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2018-02-25 07:00:20 +0100

    Initial setup of Webpack config and installation of packages + add src entry points

---
+++ webpack.base.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack'),
+    isDevelopmentEnvironment = process.env.NODE_ENV === 'dev';
+
+module.exports = {
+
+    // Tell webpack to run babel on every file it runs through
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    plugins: [
+                        'syntax-dynamic-import'
+                    ],
+                    presets: [
+                        'react',
+                        'stage-0',
+                        'flow',
+                        [
+                            'env',
+                            {
+                                targets: {
+                                    browsers: ['last 2 versions']
+                                }
+                            }
+                        ]
+                    ]
+                }
+            }
+        ]
+    },
+    mode: isDevelopmentEnvironment ? 'development' : 'production',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+                'DEBUG': JSON.stringify(process.env.DEBUG)
+            }
+        })
+    ]
+};
