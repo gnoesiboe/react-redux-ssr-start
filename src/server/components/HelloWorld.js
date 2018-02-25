96996a36d5fc4559d62339bf4e1d3ff617a89dd6
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2018-02-25 07:20:50 +0100

    Create first react component

---
+++ src/server/components/HelloWorld.js
@@ -0,0 +1,14 @@
+// @flow
+
+import React, { Component } from 'react';
+
+type Props = {};
+
+export default class HelloWorld extends Component<Props> {
+
+    render() {
+        return (
+            <h1>Hello world!</h1>
+        );
+    }
+}
