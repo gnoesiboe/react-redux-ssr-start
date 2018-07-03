7dcb55f764ddecc3923c5128cf1c20cd4710cce6
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2018-07-03 16:29:25 +0200

    Added missing home path

--- src/shared/router/routes.js
+++ src/shared/router/routes.js
@@ -5,9 +5,10 @@ import Base from '../components/base';
 import Posts, { loadData as loadPostsData } from '../components/base/components/posts';
 import NotFound from '../components/base/components/notFound';
 import RedirectHome from '../components/base/components/redirectHome/index';
-import { createPostsPath, createRedirectHomePath, createContactPath } from './urlGenerator';
+import { createPostsPath, createRedirectHomePath, createContactPath, createHomePath } from './urlGenerator';
 import Loadable from 'react-loadable';
 import Loading from '../components/base/components/loading';
+import Home from '../components/base/components/home';
 
 var LazyLoadingContact = Loadable({
     loader: () => import(/* webpackChunkName: "contact" */ '../components/base/components/contact'),
@@ -19,6 +20,11 @@ export default [
     {
         component: Base,
         routes: [
+            {
+                path: createHomePath(),
+                component: Home,
+                exact: true
+            },
             {
                 path: createPostsPath(),
                 component: Posts,
